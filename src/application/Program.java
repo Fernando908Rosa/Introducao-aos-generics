da893a9fdc19d627ca5ae8c3fae3940a46248479
fernandorosa <fernandodarosa908@gmail.com>
2022-01-03 11:38:24 -0300

    introduzindo set

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,22 @@
 package application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Program {
 
-   public static void main(String[] args) {
-	   
-	   String a = "Maria";
-	   String b = "Alex";
-	   
-	   System.out.println(a.hashCode());
-	   System.out.println(b.hashCode());
-   }
+	public static void main(String[] args) {
+		
+        Set<String> set = new HashSet<>();
+        
+        set.add("TV");
+        set.add("Notebook");
+        set.add("Tablet");
+        
+        System.out.println(set.contains("Notebook"));
+        
+        for (String p : set) {
+        	System.out.println(p);
+        }
+	}
 }
-
-		
-		
-		
-		
